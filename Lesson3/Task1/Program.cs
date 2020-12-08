49797850dd8988fc3919925fdfcc56590b8a3b7e
Andrii <ap.melnic@gmail.com>
2020-12-08 17:56:46 +0200

    Add Task 1

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
 
 namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {           
+            var file = new FileInfo(@".\Test.txt");
+            StreamWriter sw = file.CreateText();
+            sw.WriteLine("Hello world!");
+            sw.WriteLine("Hello Kyiv!");
+            sw.Close();
+
+            StreamReader sr = file.OpenText();
+            string lineFromFile = null;
+            while ((lineFromFile = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(lineFromFile);
+            }
         }
     }
 }
