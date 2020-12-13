16a40387c73e283a9780f441e554377e3026a5e4
Andrii <ap.melnic@gmail.com>
2020-12-13 17:15:33 +0200

    Add Task 2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+//Напишите шуточную программу «Дешифратор», которая бы в текстовом файле могла бы
+//заменить все предлоги на слово «ГАВ!».
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"..\..\..\Text.txt", Encoding.UTF8);
+            Console.WriteLine(text);
+            Console.WriteLine();           
+
+            string newText = Regex.Replace(text, @"\s[а-я]{1,4}\s", " ГАВ! ");
+            File.WriteAllText(@"..\..\..\Text1.txt", newText, Encoding.UTF8);
+
+            Console.WriteLine(newText);
+        }
+    }
+}
