cb9306483a22e1e33d41619f5ced4a9d292d01bb
Andrii <ap.melnic@gmail.com>
2020-12-18 15:17:58 +0200

    Add Task 2

---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.XPath;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmldoc = new XPathDocument("TelephoneBook.xml");
+            var navigator = xmldoc.CreateNavigator();
+
+            var noda = navigator.SelectSingleNode("/MyContacts/Contact/@TelephoneNumber");
+
+            Console.WriteLine($"PhoneNumber = {noda.Value}");
+        }
+    }
+}
