d340dd48a260d8445ce24e1c99f149888be347d6
Andrii <ap.melnic@gmail.com>
2020-12-27 19:05:35 +0200

    Add Task 1-2

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class CelsiusToFahrenheit
+    {
+        public CelsiusToFahrenheit()
+        {
+
+        }
+        public double ConvertTemperature(double temperature)
+        {
+            return temperature * 1.8 + 32;
+        }
+    }
+}
